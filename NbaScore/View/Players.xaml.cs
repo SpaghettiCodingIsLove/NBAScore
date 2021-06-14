a02a174d0aa9ac2aae853600a0768bbe3e076b70
Paweł Pustuła <pawel.pustula98@gmail.com>
2021-06-14 20:36:48 +0200

    wyszukiwanie zawodników

--- NbaScore/View/Players.xaml.cs
+++ NbaScore/View/Players.xaml.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using NbaScore.Model;
+
 namespace NbaScore.View
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
@@ -15,6 +17,25 @@ namespace NbaScore.View
         public Players()
         {
             InitializeComponent();
+        }
+        
+        private void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+           
+            PlayersListView.BeginRefresh();
+            
+            if (!string.IsNullOrWhiteSpace(e.NewTextValue))
+            {
+                PlayersListView.ItemsSource = HelperClass.AllPlayers.Where(x => x.FirstName.ToLower().Contains(e.NewTextValue.ToLower()) || 
+                x.LastName.ToLower().Contains(e.NewTextValue.ToLower())).OrderBy(x => x.FirstName).ThenBy(x => x.LastName);
+            }
+            else
+            {
+                PlayersListView.ItemsSource = HelperClass.AllPlayers.OrderBy(x => x.FirstName).ThenBy(x => x.LastName);
+            }
+
+            PlayersListView.EndRefresh();
+
         }
     }
 }
